747d5138a2a1e9ded7e20cc01d93069dbb63e07e
Episqol <khomchykdmitry@gmail.com>
2025-01-18 17:27:51 +0300

    Created test suite for administrative functions of SingleIdentifierID
    (cherry picked from commit 74f2e4f2cf8fbd67e4d7f0fdd413e6c1d0d58cbd)

---
+++ test-forge/Tester_SingleIdentifierID.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import {Environment_SingleIdentifierID} from "./environment/Environment_SingleIdentifierID.sol";
+import {Suite_SingleIdentifierID_Administrative} from "./suite/Suite_SingleIdentifierID_Administrative.sol";
+
+contract SingleIdentifierIDTest_Administrative is
+    Environment_SingleIdentifierID,
+    Suite_SingleIdentifierID_Administrative
+    {}
