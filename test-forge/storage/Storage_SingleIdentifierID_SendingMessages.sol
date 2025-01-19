13287c1b9bf36e6f9ed224074e6e3cd9af27ee04
rubyscore-tech <rubyscore_tech@lansoft.dev>
2025-01-20 00:55:09 +0300

    Added suit for checking message sending Created new storage helper for checking message sending added payable for Harness_SingleIdentifierID exposed functions used for message sending
    (cherry picked from commit 3d9917156c38cb06dfa43de94b33b7ec95cba895)

---
+++ test-forge/storage/Storage_SingleIdentifierID_SendingMessages.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import "lib/forge-std/src/Test.sol";
+
+import {Emitter} from "contracts/types/Structs.sol";
+import {IConnector} from "contracts/interfaces/IConnector.sol";
+import {SingleIdentifierRegistry} from "contracts/SingleIdentifierRegistry.sol";
+import {SingleRouter} from "contracts/SingleRouter.sol";
+
+import {Harness_SingleIdentifierID} from "test-forge/harness/Harness_SingleIdentifierID.sol";
+
+abstract contract Storage_SingleIdentifierID_SendingMessages is Test {
+    Harness_SingleIdentifierID public singleId;
+    SingleIdentifierRegistry public registry;
+    SingleRouter public router;
+
+    address public admin = 0x0d0D5Ff3cFeF8B7B2b1cAC6B6C27Fd0846c09361;
+    address public operator;
+    uint256 public targetChainId = 8453;
+    Emitter public emitter;
+    uint32 public connectorId = 2;
+    uint256 public fee = 2e15;
+    bytes32 public schemaId = 0xe45274bdf4f35168aa5f09f55498946a069da81f136104113e316a949ae01b37;
+
+    address public sender;
+
+    function _prepareEnv() internal virtual;
+
+    function setUp() public virtual {
+        _prepareEnv();
+
+        sender = makeAddr("sender");
+        vm.label(sender, "Sender");
+
+        emitter = Emitter(
+            keccak256("emitterId"),
+            schemaId,
+            9999999999,
+            fee,
+            block.chainid,
+            sender
+        );
+    }
+}
