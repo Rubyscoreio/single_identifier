ed6f24851a75be77cd1276bfe6652e473ed4d994
Episqol <khomchykdmitry@gmail.com>
2025-02-12 19:37:04 +0300

    Totally reworked onchain tests to get rid of almost same environments with minimal changes

--- test-forge/storage/Storage_SingleIdentifierID.sol
+++ test-forge/storage/Storage_SingleIdentifierID.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
-import "lib/forge-std/src/Test.sol";
+import {Base_Storage} from "./Base_Storage.sol";
 
 import {IConnector} from "contracts/interfaces/IConnector.sol";
 import {SingleIdentifierID} from "contracts/SingleIdentifierID.sol";
@@ -10,7 +10,7 @@ import {SingleRouter} from "contracts/SingleRouter.sol";
 
 import {Harness_SingleIdentifierID} from "test-forge/harness/Harness_SingleIdentifierID.sol";
 
-abstract contract Storage_SingleIdentifierID is Test {
+abstract contract Storage_SingleIdentifierID is Base_Storage {
     bytes32 internal constant _IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
     bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
     bytes32 public constant DEFAULT_ADMIN_ROLE = 0x00;
@@ -27,28 +27,4 @@ abstract contract Storage_SingleIdentifierID is Test {
     SingleIdentifierRegistry public registry;
     SingleRouter public router;
 
-    modifier initContracts(address _admin, address _operator, uint256 _fee) {
-//        vm.assumeNoRevert();
-        singleId.initialize(
-            _fee,
-            _admin,
-            _operator,
-            address(router)
-        );
-
-//        vm.assumeNoRevert();
-        registry.initialize(
-            _operator
-        );
-
-//        vm.assumeNoRevert();
-        router.initialize(_operator);
-        _;
-    }
-
-    function _prepareEnv() internal virtual;
-
-    function setUp() public virtual {
-        _prepareEnv();
-    }
 }
--- test-forge/storage/Storage_SingleIdentifierID_SendingMessages.sol
+++ test-forge/storage/Storage_SingleIdentifierID_SendingMessages.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
-import "lib/forge-std/src/Test.sol";
+import {Storage_SingleIdentifierID} from "./Storage_SingleIdentifierID.sol";
 
 import {Emitter} from "contracts/types/Structs.sol";
 import {IConnector} from "contracts/interfaces/IConnector.sol";
@@ -10,28 +10,20 @@ import {SingleRouter} from "contracts/SingleRouter.sol";
 
 import {Harness_SingleIdentifierID} from "test-forge/harness/Harness_SingleIdentifierID.sol";
 
-abstract contract Storage_SingleIdentifierID_SendingMessages is Test {
-    bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
-    bytes32 internal constant _IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
-
-    Harness_SingleIdentifierID public singleId;
-    SingleIdentifierRegistry public registry;
-    SingleRouter public router;
-
+abstract contract Storage_SingleIdentifierID_SendingMessages is Storage_SingleIdentifierID {
     address public admin = 0x0d0D5Ff3cFeF8B7B2b1cAC6B6C27Fd0846c09361;
     address public operator = 0x381c031bAA5995D0Cc52386508050Ac947780815;
+
     uint256 public targetChainId = 8453;
     Emitter public emitter;
-    uint32 public connectorId = 2;
+    uint32 public connectorId;
     uint256 public fee = 2e15;
     bytes32 public schemaId = 0xe45274bdf4f35168aa5f09f55498946a069da81f136104113e316a949ae01b37;
 
     address public sender;
 
-    function _prepareEnv() internal virtual;
-
-    function setUp() public virtual {
-        _prepareEnv();
+    function setUp() public override {
+        super.setUp();
 
         sender = makeAddr("sender");
         vm.label(sender, "Sender");
